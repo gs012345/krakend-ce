b602a7044443c7cef1525b7582f1e95bd9b93b85
xiaoluo <zhuhaoliang@gridsum.com>
2020-01-07 00:29:46 +0800

    增加插件

--- backend_factory.go
+++ backend_factory.go
@@ -2,26 +2,25 @@ package krakend
 
 import (
 	"context"
+	"github.com/gs012345/sso"
 
-	amqp "github.com/devopsfaith/krakend-amqp"
-	cel "github.com/devopsfaith/krakend-cel"
+	"github.com/devopsfaith/krakend-amqp"
+	"github.com/devopsfaith/krakend-cel"
 	cb "github.com/devopsfaith/krakend-circuitbreaker/gobreaker/proxy"
-	httpcache "github.com/devopsfaith/krakend-httpcache"
-	lambda "github.com/devopsfaith/krakend-lambda"
+	"github.com/devopsfaith/krakend-httpcache"
+	"github.com/devopsfaith/krakend-lambda"
 	lua "github.com/devopsfaith/krakend-lua/proxy"
 	"github.com/devopsfaith/krakend-martian"
 	metrics "github.com/devopsfaith/krakend-metrics/gin"
 	"github.com/devopsfaith/krakend-oauth2-clientcredentials"
-	opencensus "github.com/devopsfaith/krakend-opencensus"
-	pubsub "github.com/devopsfaith/krakend-pubsub"
+	"github.com/devopsfaith/krakend-opencensus"
+	"github.com/devopsfaith/krakend-pubsub"
 	juju "github.com/devopsfaith/krakend-ratelimit/juju/proxy"
 	"github.com/devopsfaith/krakend/config"
 	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
 	"github.com/devopsfaith/krakend/transport/http/client"
 	httprequestexecutor "github.com/devopsfaith/krakend/transport/http/client/plugin"
-	sso"github.com/gs012345/sso"
-
 )
 
 // NewBackendFactory creates a BackendFactory by stacking all the available middlewares:
@@ -54,7 +53,6 @@ func NewBackendFactoryWithContext(ctx context.Context, logger logging.Logger, me
 	requestExecutorFactory = httprequestexecutor.HTTPRequestExecutor(logger, requestExecutorFactory)
 	backendFactory := martian.NewConfiguredBackendFactory(logger, requestExecutorFactory)
 	bf := pubsub.NewBackendFactory(ctx, logger, backendFactory)
-	backendFactory = sso.NewConfiguredBackendFactory(logger, requestExecutorFactory)
 	backendFactory = bf.New
 	backendFactory = amqp.NewBackendFactory(ctx, logger, backendFactory)
 	backendFactory = lambda.BackendFactory(backendFactory)
@@ -64,5 +62,6 @@ func NewBackendFactoryWithContext(ctx context.Context, logger logging.Logger, me
 	backendFactory = cb.BackendFactory(backendFactory, logger)
 	backendFactory = metricCollector.BackendFactory("backend", backendFactory)
 	backendFactory = opencensus.BackendFactory(backendFactory)
+	backendFactory = sso.NewConfiguredBackendFactory(logger, requestExecutorFactory)
 	return backendFactory
 }
